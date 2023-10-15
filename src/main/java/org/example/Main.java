a03e1909288c94e9c243b89ad0d80be6ac3100d3
dermo <dermot.logue@dkit.ie>
2023-10-15 18:09:01 +0100

    renamed token array to tokens[]

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,8 +4,12 @@ package org.example;                    // Oct 2023
 // A Comma Separated Values (CSV) file is a file with text only data, where each field item (token)
 // is separated by a comma. It can be created in an Excel spreadsheet, and saved as type .CSV.
 // Each line of data in the file represents the record of one student. (One per row)
-// We read each line into a String and extract each token into a String array using String.split().
-// Finally, we convert each String in the array into its correct type: (String), int or double, as required.
+// We read each line into a String and then split the line into individual tokens/fields, and
+// add each field value as an element into a String array (called data below)
+//
+// Finally, we access each String token in the array using the appropriate getter to
+// parse it into the correct data type and assign it to a variable.
+// (String), int or double, as required.
 
 import java.io. * ;
 import java.util.Scanner;
@@ -27,15 +31,15 @@ public class Main {
             // read one line at a time into a String, and parse the String into tokens (parts)
             while (sc.hasNextLine())
             {
-                String line = sc.nextLine();             // read full line ( ending with a "\n" )
-                String [] data = line.split(",");  // split string using a comma as the delimiter (separator)
+                String line = sc.nextLine();             // read full line ( delimited by a "\n" )
+                String [] tokens = line.split(",");  // split line using a comma as the delimiter (separator)
 
-                String name = data[0];  // extract first token/data from the string array (i.e. the name)
-                int age = Integer.parseInt(data[1]);  // e.g. Convert String "19" to int value 19
-                double height = Double.parseDouble(data[2]);  // e.g. Convert String "1.82" to double 1.82
-                double gpa = Double.parseDouble(data[3]);
+                String name = tokens[0];  // extract first token/field from the tokens array (i.e. the name)
+                int age = Integer.parseInt(tokens[1]);  // e.g. Convert String "19" to int value 19
+                double height = Double.parseDouble(tokens[2]);  // e.g. Convert String "1.82" to double 1.82
+                double gpa = Double.parseDouble(tokens[3]);
 
-                // Print out the row of data using format specifiers
+                // Print out the row of field values using format specifiers
                 System.out.printf("%-20s %5d %5.2f %5.2f %n",name,age,height,gpa);
             }
 
